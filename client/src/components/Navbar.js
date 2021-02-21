70a3ec7a6a992a89aa9ebdf1eb8043471950edbb
Lyly Long <lylylong@gmail.com>
2021-02-21 17:18:59 -0500

    add search function for most recent movies

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 import SignUpForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 import Auth from "../utils/auth";
@@ -10,67 +10,120 @@ import {
   Text,
   Stack,
   Flex,
-  MenuIcon,
   useColorModeValue,
+  useDisclosure,
+  Button,
+  // Lorem,
+  // Modal,
+  // ModalOverlay,
+  // ModalContent,
+  // ModalHeader,
+  // ModalFooter,
+  // ModalBody,
+  // ModalCloseButton,
 } from "@chakra-ui/react";
 
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
 const AppNavbar = () => {
   // set modal display state
-  // const [showModal, setShowModal] = useState(false);
-  const [isOpen, setIsOpen] = React.useState(false);
-  // const { isOpen, onToggle } = useDisclosure();
-  const toggle = () => setIsOpen(!isOpen);
+  const [showModal, setShowModal] = useState(false);
+  const [isOpenToggle, setIsOpen] = React.useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = React.useRef();
+  const toggle = () => setIsOpen(!isOpenToggle);
 
   return (
-    <Box>
-      <Flex
-        as="nav"
-        align="center"
-        justify="space-between"
-        wrap="wrap"
-        w="100%"
-        mb={8}
-        p={8}
-        bg={useColorModeValue("white", "gray.800")}
-        color={useColorModeValue("gray.600", "white")}
-      >
-        <Box w="100px" color={["primary.500", "primary.500"]}>
-          <Text fontSize="lg" fontWeight="bold" as={Link} to="/">
-            Moviegoer
-          </Text>
-        </Box>
-
-        <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-          {isOpen ? <CloseIcon /> : <HamburgerIcon />}
-        </Box>
-
-        <Box
-          display={{ base: isOpen ? "block" : "none", md: "block" }}
-          flexBasis={{ base: "100%", md: "auto" }}
+    <>
+      <Box>
+        <Flex
+          as="nav"
+          align="center"
+          justify="space-between"
+          wrap="wrap"
+          w="100%"
+          p={8}
+          bg={useColorModeValue("white", "gray.800")}
+          color={useColorModeValue("gray.600", "white")}
         >
-          <Stack
-            spacing={8}
-            align="center"
-            justify={["center", "space-between", "flex-end", "flex-end"]}
-            direction={["column", "row", "row", "row"]}
-            pt={[4, 4, 0, 0]}
+          <Box w="100px" color={["primary.500", "primary.500"]}>
+            <Text fontSize="lg" fontWeight="bold" as={Link} to="/">
+              Moviegoer
+            </Text>
+          </Box>
+
+          <Box display={{ base: "block", md: "none" }} onClick={toggle}>
+            {isOpenToggle ? <CloseIcon /> : <HamburgerIcon />}
+          </Box>
+
+          <Box
+            display={{ base: isOpenToggle ? "block" : "none", md: "block" }}
+            flexBasis={{ base: "100%", md: "auto" }}
           >
-            <Link>
-              <Text display="block" as={Link} to="/">
-                Home
-              </Text>
-            </Link>
-            <Link>
-              <Text display="block" as={Link} to="/saved">
-                Your Movies
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
-      </Flex>
-    </Box>
+            <Stack
+              spacing={8}
+              align="center"
+              justify={["center", "space-between", "flex-end", "flex-end"]}
+              direction={["column", "row", "row", "row"]}
+              pt={[4, 4, 0, 0]}
+            >
+              <Link>
+                <Text display="block" as={Link} to="/">
+                  Home
+                </Text>
+              </Link>
+
+              {/* if user is logged in show saved movies and logout */}
+              {Auth.loggedIn() ? (
+                <>
+                  <Link>
+                    <Text display="block" as={Link} to="/saved">
+                      Your Watchlist
+                    </Text>
+                  </Link>
+                  <Link onClick={Auth.logout}>Logout</Link>
+                </>
+              ) : (
+                <Link onClick={() => setShowModal(true)}>Login/Sign Up</Link>
+              )}
+            </Stack>
+          </Box>
+        </Flex>
+      </Box>
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
+                <Nav.Item>
+                  <Nav.Link eventKey="login">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignUpForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+      ;
+    </>
   );
 };
 
--- client/src/pages/SearchMovies.js
+++ client/src/pages/SearchMovies.js
@@ -17,8 +17,97 @@ import { saveMovieIds, getSavedMovieIds } from "../utils/localStorage";
 import Auth from "../utils/auth";
 
 const SearchMovies = () => {
+  const [displayMovies, setDisplayMovies] = useState([
+    {
+      movieId: 577922,
+      releaseDate: "2020-08-22",
+      title: "Tenet",
+      description:
+        "Armed with only one word - Tenet - and fighting for the survival of the entire world, the Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+      image: "https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg",
+    },
+    {
+      movieId: 602269,
+      releaseDate: "2021-01-28",
+      title: "The Little Things",
+      description: `Deputy Sheriff Joe "Deke" Deacon joins forces with Sgt. Jim Baxter to search for a serial killer who's terrorizing Los Angeles. As they track the culprit, Baxter is unaware that the investigation is dredging up echoes of Deke's past, uncovering disturbing secrets that could threaten more than his case.`,
+      image: "https://image.tmdb.org/t/p/w500/c7VlGCCgM9GZivKSzBgzuOVxQn7.jpg",
+    },
+    {
+      movieId: 587807,
+      releaseDate: "2021-02-11",
+      title: "Tom & Jerry",
+      description:
+        "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can't evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse.",
+      image: "https://image.tmdb.org/t/p/w500/e06BpqZIxRSpvNSbItcGcgs0S5I.jpg",
+    },
+  ]);
+
+  function handleLoad() {
+    const https = require("https");
+    const url =
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=018c380ce92d45e85123258d739abb6e&language=en-US&page=1";
+    https.get(url, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        data = JSON.parse(data);
+        const movieDisplay = data.results.map((movie) => ({
+          movieId: movie.id,
+          releaseDate:
+            movie.release_date || movie.publishedDate || "No release date",
+          title: movie.title,
+          description: movie.overview,
+          image:
+            `https://image.tmdb.org/t/p/w500${movie.poster_path}` ||
+            `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` ||
+            "",
+        }));
+        setDisplayMovies(movieDisplay);
+        console.log(movieDisplay);
+        // return movieDisplay;
+      });
+    });
+  }
+
   // create state for holding returned google api data
   const [searchedMovies, setSearchedMovies] = useState([]);
+
+  // initial search
+  // const handleLoad = async (event) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.themoviedb.org/3/movie/now_playing?api_key=018c380ce92d45e85123258d739abb6e&language=en-US&page=1`
+  //     );
+
+  //     // if (!response.ok) {
+  //     //   throw new Error("something went wrong!");
+  //     // }
+  //     const { results } = await response.json();
+  //     const movieData = results.map((movie) => ({
+  //       movieId: movie.id,
+  //       releaseDate:
+  //         movie.release_date || movie.publishedDate || "No release date",
+  //       title: movie.title,
+  //       description: movie.overview,
+  //       image:
+  //         `https://image.tmdb.org/t/p/w500${movie.poster_path}` ||
+  //         `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` ||
+  //         "",
+  //     }));
+
+  //     setSearchedMovies(movieData);
+  //     console.log(movieData);
+  //     // setSearchInput("");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState("");
 
@@ -28,7 +117,6 @@ const SearchMovies = () => {
   const [saveMovie, { error }] = useMutation(SAVE_MOVIE);
 
   // set up useEffect hook to save `savedMovieIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveMovieIds(savedMovieIds);
   });
@@ -75,9 +163,9 @@ const SearchMovies = () => {
   // create function to handle saving a movie to our database
   const handleSaveMovie = async (movieId) => {
     // find the movie in `searchedMovies` state by the matching id
-    const movieToSave = searchedMovies.find(
-      (movie) => movie.movieId === movieId
-    );
+    const movieToSave =
+      searchedMovies.find((movie) => movie.movieId === movieId) ||
+      displayMovies.find((movie) => movie.movieId === movieId);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -93,7 +181,7 @@ const SearchMovies = () => {
         // user inputs
         variables: { movieData: { ...movieToSave } },
       });
-      console.log(savedMovieIds);
+      // console.log(savedMovieIds);
       // take the token and will set it to localStorage
       setSavedMovieIds([...savedMovieIds, movieToSave.movieId]);
     } catch (error) {
@@ -105,7 +193,7 @@ const SearchMovies = () => {
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Search for Movies!</h1>
+          <h1>Search for Movie by name!</h1>
           <Form onSubmit={handleFormSubmit}>
             <Form.Row>
               <Col xs={12} md={8}>
@@ -122,6 +210,14 @@ const SearchMovies = () => {
                 <Button type="submit" variant="success" size="lg">
                   Submit Search
                 </Button>
+                <Button
+                  type="submit"
+                  variant="success"
+                  size="lg"
+                  onClick={handleLoad}
+                >
+                  Now Playing
+                </Button>
               </Col>
             </Form.Row>
           </Form>
@@ -132,42 +228,76 @@ const SearchMovies = () => {
         <h2>
           {searchedMovies.length
             ? `Viewing ${searchedMovies.length} results:`
-            : "Search for a movie to begin"}
+            : "Example search results, search for more movies!"}
         </h2>
         <CardColumns>
-          {searchedMovies.map((movie) => {
-            return (
-              <Card key={movie.movieId} border="dark">
-                {movie.image ? (
-                  <Card.Img
-                    src={movie.image}
-                    alt={`The cover for ${movie.title}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{movie.title}</Card.Title>
-                  <p className="small">Release Date: {movie.releaseDate}</p>
-                  <Card.Text>{movie.description}</Card.Text>
-                  {Auth.loggedIn() && (
-                    <Button
-                      disabled={savedMovieIds?.some(
-                        (savedMovieId) => savedMovieId === movie.movieId
+          {searchedMovies.length
+            ? searchedMovies.map((movie) => {
+                return (
+                  <Card key={movie.movieId} border="dark">
+                    {movie.image ? (
+                      <Card.Img
+                        src={movie.image}
+                        alt={`The cover for ${movie.title}`}
+                        variant="top"
+                      />
+                    ) : null}
+                    <Card.Body>
+                      <Card.Title>{movie.title}</Card.Title>
+                      <p className="small">Release Date: {movie.releaseDate}</p>
+                      <Card.Text>{movie.description}</Card.Text>
+                      {Auth.loggedIn() && (
+                        <Button
+                          disabled={savedMovieIds?.some(
+                            (savedMovieId) => savedMovieId === movie.movieId
+                          )}
+                          className="btn-block btn-info"
+                          onClick={() => handleSaveMovie(movie.movieId)}
+                        >
+                          {savedMovieIds?.some(
+                            (savedMovieId) => savedMovieId === movie.movieId
+                          )
+                            ? "Saved!"
+                            : "Add to Watchlist!"}
+                        </Button>
                       )}
-                      className="btn-block btn-info"
-                      onClick={() => handleSaveMovie(movie.movieId)}
-                    >
-                      {savedMovieIds?.some(
-                        (savedMovieId) => savedMovieId === movie.movieId
-                      )
-                        ? "Already been saved!"
-                        : "Save this Movie!"}
-                    </Button>
-                  )}
-                </Card.Body>
-              </Card>
-            );
-          })}
+                    </Card.Body>
+                  </Card>
+                );
+              })
+            : displayMovies.map((movie) => {
+                return (
+                  <Card key={movie.movieId} border="dark">
+                    {movie.image ? (
+                      <Card.Img
+                        src={movie.image}
+                        alt={`The cover for ${movie.title}`}
+                        variant="top"
+                      />
+                    ) : null}
+                    <Card.Body>
+                      <Card.Title>{movie.title}</Card.Title>
+                      <p className="small">Release Date: {movie.releaseDate}</p>
+                      <Card.Text>{movie.description}</Card.Text>
+                      {Auth.loggedIn() && (
+                        <Button
+                          disabled={savedMovieIds?.some(
+                            (savedMovieId) => savedMovieId === movie.movieId
+                          )}
+                          className="btn-block btn-info"
+                          onClick={() => handleSaveMovie(movie.movieId)}
+                        >
+                          {savedMovieIds?.some(
+                            (savedMovieId) => savedMovieId === movie.movieId
+                          )
+                            ? "Saved!"
+                            : "Add to Watchlist!"}
+                        </Button>
+                      )}
+                    </Card.Body>
+                  </Card>
+                );
+              })}
         </CardColumns>
       </Container>
     </>
