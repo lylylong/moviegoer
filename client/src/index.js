0b266a7b90fc3b7cbef801056906f6df8015f7e5
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2021-02-23 11:02:21 -0500

    installing chakra UI & initial component structure

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from "@chakra-ui/react"
 
 ReactDOM.render(
+  <ChakraProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ChakraProvider>,
   document.getElementById('root')
 );
